d2e665a722ccdc9382b4b4aa289cc77592caa5d5
João Faria <Joao.Faria@astro.up.pt>
2013-09-09 11:29:30 +0100

    renaming

---
+++ OPEN/_OPENversion.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2013 João Faria
+# This file is part of OPEN which is licensed under the MIT license.
+# You should have received a copy of the license along with OPEN. See LICENSE.
+#
+""" Provides version information for OPEN. """
+
+__version_info__ = (0, 0, 1) 
+__version__ = '.'.join(map(str, __version_info__))
