c4687f39546720971255b76c5d82b8e0de925ba0
João Faria <Joao.Faria@astro.up.pt>
2013-10-03 18:38:09 +0100

    until I find a way to do semilogx with pyqtgraph, will use matplotlib

--- OPEN/periodograms.py
+++ OPEN/periodograms.py
@@ -224,16 +224,17 @@ class gls(PeriodogramBase):
     """
       Create a plot.
     """
-    # self.fig = plt.figure()
-    # self.ax = self.fig.add_subplot(1,1,1)
-    # self.ax.set_title("Normalized periodogram")
-    # self.ax.set_xlabel("Period")
-    # self.ax.set_ylabel("Power")
-    # self.ax.semilogx(1./self.freq, self.power, 'b-')
-    # plt.tight_layout()
-    # plt.show()
-    pg.plot(1./self.freq, self.power, title="Periodogram")
-    pg.QtGui.QApplication.exec_()
+    self.fig = plt.figure()
+    self.ax = self.fig.add_subplot(1,1,1)
+    self.ax.set_title("Normalized periodogram")
+    self.ax.set_xlabel("Period")
+    self.ax.set_ylabel("Power")
+    self.ax.semilogx(1./self.freq, self.power, 'b-')
+    plt.tight_layout()
+    plt.show()
+    # p = pg.plot(1./self.freq, self.power, title="Periodogram")
+    # p.plotItem.setLogMode(1./self.freq, self.power)
+    # pg.QtGui.QApplication.exec_()
 
   def _output(self, verbose=False):
     """
