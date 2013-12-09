d9a8289498c8c032466d05c97124d970e12d7590
João Faria <joao.faria@astro.up.pt>
2013-12-09 17:04:52 +0000

    rectangle-selectable plot (experimental)

--- OPEN/utils.py
+++ OPEN/utils.py
@@ -40,3 +40,40 @@ def stdout_write(msg):
 	Useful to print in the same line """
 	stdout.write(msg)
 	stdout.flush()
+
+
+
+### Matplotlib advanced plot interaction stuff
+from matplotlib.widgets import RectangleSelector
+import numpy as np
+import matplotlib.pyplot as plt
+
+def line_select_callback(eclick, erelease):
+    'eclick and erelease are the press and release events'
+    x1, y1 = eclick.xdata, eclick.ydata
+    x2, y2 = erelease.xdata, erelease.ydata
+    print ("(%3.2f, %3.2f) --> (%3.2f, %3.2f)" % (x1, y1, x2, y2))
+    print (" The button you used were: %s %s" % (eclick.button, erelease.button))
+
+def toggle_selector(event):
+    print (' Key pressed.')
+    if event.key in ['Q', 'q'] and toggle_selector.RS.active:
+        print (' RectangleSelector deactivated.')
+        toggle_selector.RS.set_active(False)
+    if event.key in ['A', 'a'] and not toggle_selector.RS.active:
+        print (' RectangleSelector activated.')
+        toggle_selector.RS.set_active(True)
+
+def selectable_plot(*args, **kwargs):
+    fig, current_ax = plt.subplots()                    # make a new plotingrange
+
+    plt.plot(*args, **kwargs)  # plot something
+
+    # drawtype is 'box' or 'line' or 'none'
+    toggle_selector.RS = RectangleSelector(current_ax, line_select_callback,
+                                           drawtype='box', useblit=True,
+                                           button=[1,3], # don't use middle button
+                                           minspanx=5, minspany=5,
+                                           spancoords='pixels')
+    plt.connect('key_press_event', toggle_selector)
+    plt.show()
