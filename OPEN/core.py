fc9cdab7016af652e5af5c2d137cce3e7f84d809
João Faria <joao.faria@astro.up.pt>
2013-12-16 11:32:11 +0000

    cleanup dawson fabrycky

--- OPEN/core.py
+++ OPEN/core.py
@@ -285,85 +285,49 @@ def do_correlate(system, vars=(), verbose=False):
 def do_Dawson_Fabrycky(system):
 	time, rv, err = system.time, system.vrad, system.error
 	ofac = 2.0
-	# Make permutations for indices of length n
-
-	def permutate(n):
-
-		A=[]
-		for i in range(n):
-			a = int(n*random.random())
-			if a==n: a=n-1
-			A.append(a)
-
-		return A
-
-
-	# Calculate the spectral window function and its phase angles
 
 	def specwindow(freq,time):
-
+		""" Calculate the spectral window function and its phase angles """
 		n = len(time)
 		W = [sum([exp(-2.j*pi*f*t) for t in time])/float(n) for f in freq]
-
 		amp = [sqrt(t.real*t.real + t.imag*t.imag) for t in W]
 		phase = [arctan2(t.imag,t.real) for t in W]
-
 		return amp,phase
 
-
 	def fitcirc(per,K,Tc,gam,t):
-
 		phase = 2*pi*Tc/per
-
 		omega = 2. * pi / per
-
 		rv = K * cos(omega * t - phase) + gam
-
 		return rv
 
-
-
 	def fitkep(per,ecc,pomega,K,T0,gam,t):
-
-
 		afit = K*cos(pomega/180*pi)
 		bfit = -K*sin(pomega/180*pi)
 		cfit = K*ecc*cos(pomega/180*pi) + gam
-
 		M=(t-T0)/per*2.*pi
-
 		sE = 0.
 		sEtemp = 2.
 		while abs(sE - sEtemp) > 0.05:
-
 			sEtemp = sE
 			EA = ecc * sE + M
 			sE = sin(EA)
-			
 		cE = cos(EA)
-
 		cosnu = (cE - ecc) / (1. - ecc * cE)
 		sinnu = sqrt(1. - ecc * ecc) * sE / (1. - ecc * cE)
-
 		rv = afit * cosnu + bfit * sinnu + cfit
-
 		return rv
 
 	plow = 0.5
-
 	n = len(err)
 
 	### GET THE REAL PERIODOGRAM
 	freq,power,a_cos,b_sin,c_cte,phi,fNy,xdif = periodogram_DF(array(time),array(rv),array(err),ofac,plow)
-
 	### GET THE WINDOW FUNCTION AT THOSE FREQUENCIES + plot dials
 	amp,phase = specwindow(freq,time)
 
 	figure(num=1,figsize=(11.69,8.27))
 
-
 	#### GET 3 Maximum peaks + create fake data
-
 	nf = len(freq)
 	temp = sorted(zip(power,freq,a_cos,b_sin,c_cte,phi))
 	fmax1 = (temp[nf-1])[1]
@@ -425,42 +389,12 @@ def do_Dawson_Fabrycky(system):
 	semilogx([1./fmax2,1./fmax2+0.025*cos(ph2)],[max(power)+0.1,max(power)+0.1+0.025*sin(ph2)],'k-',lw=1)
 	semilogx([1./fmax3,1./fmax3+0.025*cos(ph3)],[max(power)+0.1,max(power)+0.1+0.025*sin(ph3)],'k-',lw=1)
 
-
 	xlim(plow,xdiff*ofac)
 	ylim(0.0,max(power)+0.2)
 
 	### PLOT FAKE PERIODOGRAMS + DIALS
-
 	#### 1st FAKE
-
 	freq,power,a_cos,b_sin,c_cte,phi,fNy,xdif = periodogram_DF(array(timefake),array(rv_fake1),array(errfake),ofac,plow)
-
-	#nf = len(freq)
-	#temp = sorted(zip(power,freq,a_cos,b_sin,c_cte,phi))
-	#fmax1 = (temp[nf-1])[1]
-
-	#fmax2 = fmax1
-	#i = 1
-	#while abs(fmax2 - fmax1) < 0.01:
-		
-	#	i += 1
-	#	fmax2 = (temp[nf-i])[1]
-
-	#fmax3 = fmax2
-
-	#j = i
-	#while abs(fmax3 - fmax2) < 0.01 or abs(fmax3 - fmax1) < 0.01:
-		
-	#	j += 1
-	#	fmax3 = (temp[nf-j])[1]
-
-
-	#print 'Maximum frequencies at: ',fmax1,fmax2,fmax3
-	#print 'That is periods at: ',round(1./fmax1,2),round(1./fmax2,2),round(1./fmax3,2)
-
-	#ph1 = (temp[nf-1])[5]
-	#ph2 = (temp[nf-i])[5]
-	#ph3 = (temp[nf-j])[5]
 
 	ind1 = list(freq).index(fmax1)
 	ind2 = list(freq).index(fmax2)
@@ -478,15 +412,11 @@ def do_Dawson_Fabrycky(system):
 	semilogx([1./fmax2,1./fmax2+0.045*cos(phi[ind2])],[max(power)+0.1,max(power)+0.1+0.045*sin(phi[ind2])],'k-',lw=1)
 	semilogx([1./fmax3,1./fmax3+0.045*cos(phi[ind3])],[max(power)+0.1,max(power)+0.1+0.045*sin(phi[ind3])],'k-',lw=1)
 
-
 	xlim(plow,xdiff*ofac)
 	ylim(0.0,max(power)+0.2)
 
-
 	#### 2nd FAKE
-
 	freq,power,a_cos,b_sin,c_cte,phi,fNy,xdif = periodogram_DF(array(timefake),array(rv_fake2),array(errfake),ofac,plow)
-
 
 	ind1 = list(freq).index(fmax1)
 	ind2 = list(freq).index(fmax2)
@@ -508,14 +438,11 @@ def do_Dawson_Fabrycky(system):
 	ylim(0.0,max(power)+0.2)
 
 	#### 3rd FAKE
-
 	freq,power,a_cos,b_sin,c_cte,phi,fNy,xdif = periodogram_DF(array(timefake),array(rv_fake3),array(errfake),ofac,plow)
-
 
 	ind1 = list(freq).index(fmax1)
 	ind2 = list(freq).index(fmax2)
 	ind3 = list(freq).index(fmax3)
-
 
 	subplot(4,1,4)
 
@@ -532,7 +459,5 @@ def do_Dawson_Fabrycky(system):
 	xlim(plow,xdiff*ofac)
 	ylim(0.0,max(power)+0.2)
 
-
 	# savefig(name+'_DF.ps',orientation = 'Landscape')
 	show()
-	# close()
