d80f60e8c1fb6cd49fd25c37a9773a62ca1c8548
João Faria <joao.faria@astro.up.pt>
2014-09-24 16:12:11 +0100

    adds sort function

--- OPEN/multinest/array_utils.f90
+++ OPEN/multinest/array_utils.f90
@@ -132,5 +132,76 @@ function linspace(xmin, xmax, num) result(x)
     end if
 end function linspace
 
+subroutine sort(A, N)
+	! arguments
+	integer, intent(in) :: N
+	real(kind=8), dimension(N), intent(inout) :: A
+	! local variables
+	real(kind=8), dimension((N+1)/2) :: T
+
+	call MergeSort(A,N,T)
+
+	contains 
+		subroutine Merge(A,NA,B,NB,C,NC)
+		
+			integer, intent(in) :: NA,NB,NC         ! Normal usage: NA+NB = NC
+			real(kind=8), intent(in out) :: A(NA)        ! B overlays C(NA+1:NC)
+			real(kind=8), intent(in)     :: B(NB)
+			real(kind=8), intent(in out) :: C(NC)
+
+			integer :: I,J,K
+		 
+			I = 1; J = 1; K = 1;
+			do while(I <= NA .and. J <= NB)
+				if (A(I) <= B(J)) then
+					C(K) = A(I)
+					I = I+1
+				else
+			 		C(K) = B(J)
+					J = J+1
+				endif
+				K = K + 1
+			enddo
+			do while (I <= NA)
+				C(K) = A(I)
+				I = I + 1
+				K = K + 1
+			enddo
+			return
+		end subroutine merge
+ 
+		recursive subroutine MergeSort(A,N,T)
+			integer, intent(in) :: N
+			real(kind=8), dimension(N), intent(in out) :: A
+			real(kind=8), dimension((N+1)/2), intent (out) :: T
+
+			integer :: NA, NB
+			real(kind=8) :: V
+		 
+			if (N < 2) return
+			if (N == 2) then
+				if (A(1) > A(2)) then
+					V = A(1)
+					A(1) = A(2)
+					A(2) = V
+				endif
+				return
+			endif      
+			NA=(N+1)/2
+			NB=N-NA
+
+			call MergeSort(A,NA,T)
+			call MergeSort(A(NA+1),NB,T)
+
+			if (A(NA) > A(NA+1)) then
+				T(1:NA)=A(1:NA)
+				call Merge(T,NA,A(NA+1),NB,A,N)
+			endif
+			return
+		end subroutine MergeSort
+ 
+end subroutine sort
+
+
 
 end module array_utils
