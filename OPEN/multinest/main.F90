3d0b38d75b75bb4914773bba89346783ed7ef990
João Faria <joao.faria@astro.up.pt>
2015-05-05 13:36:27 +0100

    Give namelist filename as an input to the nest command and also as the first command line argument to the nest executable. This allows many simultaneous jobs.

--- OPEN/multinest/main.F90
+++ OPEN/multinest/main.F90
@@ -10,10 +10,11 @@ program main
 
     implicit none
 
-    integer i
-    integer :: iou, ierr
+    integer :: i, iou, ierr
     integer :: PGOPEN
     integer :: n
+
+    character(len=100) cmdline
     character(len=40) line
     character(len=1) delim
     character(len=5), dimension(10) :: args
@@ -29,26 +30,29 @@ program main
                                training, trained_parameters, trained_std, &
                                lin_dep, n_lin_dep
 
-    ! read configuration values from namelist
-    ! CWD is set by the preprocessor to the directory where this file main.F90 lives
-    iou = 8
-    open(unit=iou, file=CWD//"/namelist1", status='old', action='read', delim='quote', iostat=ierr)
-    if (ierr /= 0) then
-      stop 'Failed to open namelist file'
-    else
-      read(iou, nml=NEST_parameters)
-      close(iou)
-    end if
-
-
-#ifdef MPI
     !MPI initializations
+#ifdef MPI
     call MPI_INIT(ierr)
     if (ierr/=MPI_SUCCESS) then
             write(*,*)'Error starting MPI. Terminating.'
             call MPI_ABORT(MPI_COMM_WORLD,ierr)
     end if
 #endif
+
+    !! read configuration values from namelist
+    ! namelist file from first cmd line argument
+    call get_command_argument(1, cmdline, status=ierr)
+    if (ierr /= 0) error stop 'Failed to retrieve namelist filename from cmd line'
+
+    iou = 8
+    open(unit=iou, file=trim(cmdline), status='old', action='read', delim='quote', iostat=ierr)
+    if (ierr /= 0) then
+      error stop 'Failed to open namelist file'
+    else
+      read(iou, nml=NEST_parameters)
+      close(iou)
+    end if
+
 
 #ifdef PLOT 
     ! initialize graphics device
