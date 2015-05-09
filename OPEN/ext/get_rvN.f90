a9014a6d31497e8488e9cb144e84a492c39ead60
João Faria <joao.faria@astro.up.pt>
2015-05-09 14:57:44 +0100

    double precision default

--- OPEN/ext/get_rvN.f90
+++ OPEN/ext/get_rvN.f90
@@ -23,16 +23,13 @@ subroutine get_rvN(time, P, K, ecc, omega, t0, vsys, vel, nt, np)
 ! Input arguments
     integer nt, np ! number of observations, planets
     real (kind=8), dimension(nt) :: time, vel
-    real (kind=8), dimension(np) :: p, k, ecc, omega, t0
+    real (kind=8), dimension(np) :: P, K, ecc, omega, t0
     real (kind=8) :: vsys
 
 ! Local variables
-    !integer,parameter :: sp = selected_real_kind(p=6,r=37)
-    integer,parameter :: dp = selected_real_kind(p=15,r=307)
     integer :: i
-
-    real(dp), parameter :: pi = 3.1415926535897932384626433832795029_dp
-    real(dp), parameter :: twopi = 2.0_dp * pi 
+    real(kind=8), parameter :: pi = 3.1415926535897932384626433832795029d0
+    real(kind=8), parameter :: twopi = 2.0d0 * pi 
 
 !     vel = vsys
     do i=1,np
@@ -43,11 +40,11 @@ subroutine get_rvN(time, P, K, ecc, omega, t0, vsys, vel, nt, np)
 
 contains
   
-  elemental real(dp) function rv_curve(time, P, K, ecc, omega, t0) result(vel)
+  elemental real(kind=8) function rv_curve(time, P, K, ecc, omega, t0) result(vel)
   ! RV curve for one planet - see Balan & Lahav (2009), MNRAS, 394, 1936
-    real(dp), intent(in) :: time
-    real(dp), intent(in) :: P, K, ecc, omega, t0
-    real(dp) :: M, f 
+    real(kind=8), intent(in) :: time
+    real(kind=8), intent(in) :: P, K, ecc, omega, t0
+    real(kind=8) :: M, f 
 
     M = twopi * (time-t0)/P
     f = true_anom(M, ecc)
@@ -57,9 +54,9 @@ contains
   end function rv_curve
 
 
-  elemental real(dp) function true_anom(M, ecc) result(nu)
-    real(dp), intent(in) :: M, ecc
-    real(dp) :: E, cosE
+  elemental real(kind=8) function true_anom(M, ecc) result(nu)
+    real(kind=8), intent(in) :: M, ecc
+    real(kind=8) :: E, cosE
 
     E = ecc_anom(M, ecc)
 
@@ -77,11 +74,11 @@ contains
   
 
 
-  elemental real(dp) function ecc_anom(M, ecc) result(E)
-    real(dp), intent(in) :: M, ecc
-    real(dp), parameter :: derror = 1.0d-7
+  elemental real(kind=8) function ecc_anom(M, ecc) result(E)
+    real(kind=8), intent(in) :: M, ecc
+    real(kind=8), parameter :: derror = 1.0d-7
     integer :: iter
-    real(dp) :: EA_new, EA_old
+    real(kind=8) :: EA_new, EA_old
     
     ! catch special case
     if (ecc .eq. 0.d0) then
@@ -90,7 +87,7 @@ contains
     endif
 
     iter = 0
-    EA_old = 0.5_dp * pi 
+    EA_old = 0.5d0 * pi 
     if (ecc < 0.8d0) then
       EA_new = newton(EA_old, ecc, M)
       do while (abs(EA_old - EA_new) >= derror .and. iter<200)
@@ -111,14 +108,14 @@ contains
   end function ecc_anom
         
         
-  real(dp) function halley(EA, e, M) result(EA_new)
-    real(dp), intent(in) :: EA, e, M
-    real(dp) :: spsi0, f, fp, fpp
+  real(kind=8) function halley(EA, e, M) result(EA_new)
+    real(kind=8), intent(in) :: EA, e, M
+    real(kind=8) :: spsi0, f, fp, fpp
 
     ! Compute the function and derivatives.
     spsi0 = sin(EA)
     f = EA - e * spsi0 - M
-    fp = 1._dp - e * cos(EA)
+    fp = 1.d0 - e * cos(EA)
     fpp = e * spsi0
 
     ! Take a second order step.
@@ -126,26 +123,26 @@ contains
 
     ! Deal with looping boundaries properly.
     if (EA_new .gt. twopi) then
-      EA_new = 0.5 * (EA + twopi)
-    elseif (EA_new .lt. 0.0) then
-      EA_new = 0.5 * EA 
+      EA_new = 0.5d0 * (EA + twopi)
+    elseif (EA_new .lt. 0.0d0) then
+      EA_new = 0.5d0 * EA 
     endif
 
   end function halley
         
-  elemental real(dp) function strict_iteration(EA, e, M) result(EA_new)
+  elemental real(kind=8) function strict_iteration(EA, e, M) result(EA_new)
   ! Solve kepler's equation by a simple iteration scheme
-    real(dp), intent(in) :: EA, e, M
+    real(kind=8), intent(in) :: EA, e, M
     EA_new = M+e*sin(EA)
   end function strict_iteration
         
-  elemental real(dp) function newton(EA, e, M) result(EA_new)
+  elemental real(kind=8) function newton(EA, e, M) result(EA_new)
   ! The following routine is used to solve kepler's equation using
   ! Newton's method. It is very fast and reliable for small values of
   ! e, but can be wildly erratic for e close to 1. See, e.g, the discussion
   ! in Jean Meeus, Astronomical Algorithms, Willmann-Bell, 1991, 181-193.
-    real(dp), intent(in) :: EA, e, M
-    EA_new = EA + (M + e*sin(EA) - EA)/(1 - e*cos(EA))
+    real(kind=8), intent(in) :: EA, e, M
+    EA_new = EA + (M + e*sin(EA) - EA)/(1d0 - e*cos(EA))
   end function newton
   
   
