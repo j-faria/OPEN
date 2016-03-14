fix issues with variance calculation and star name finder

OPEN/utils.py
@@ -91,7 +91,7 @@ def wstd(array, weights=None, biased=False):
     corrected for small samples (if biased=False). 
     """
     x = np.atleast_1d(array)
-    xvar = var(array, weights=weights, biased=biased)
+    xvar = var(x, weights=weights, biased=biased)
     return np.sqrt(xvar)
 
 
@@ -153,7 +153,10 @@ def get_star_name(system):
     else:
         full_path = system.provenance.keys()[0]
         bn = os.path.basename(full_path)
-        star = findall('HD\d+|HIP\d+', bn)[0]
+        try:
+            star = findall('HD\d+|HIP\d+', bn)[0]
+        except IndexError:
+            star = 'unknown'
         # i = bn.rfind('_harps_mean_corr.rdb')
         # if i == -1:
         #     i = bn.rfind('_harps_mean.rdb')
@@ -165,12 +168,17 @@ def get_star_name(system):
 def get_star_name_from_path(full_path):
     """ Return the name of the star (works for standard HARPS filenames) """
     bn = os.path.basename(full_path)
-    i = bn.rfind('_harps_mean_corr.rdb')
-    if i == -1:
-        i = bn.rfind('_harps_mean.rdb')
-    if i == -1:
-        i = bn.rfind('_harps.rdb')
-    star = bn[:i]
+    try:
+        star = findall('HD\d+|HIP\d+|BD-\d+|CD-\d+', bn)[0]
+    except IndexError:  # didn't find correct name
+        star = None
+
+    # i = bn.rfind('_harps_mean_corr.rdb')
+    # if i == -1:
+    #     i = bn.rfind('_harps_mean.rdb')
+    # if i == -1:
+    #     i = bn.rfind('_harps.rdb')
+    # star = bn[:i]
     return star
 
 ## code from http://en.wikibooks.org/wiki/Algorithm_Implementation/Strings/Longest_common_substring